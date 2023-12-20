ad7a251257a35755ba078e2051d3d27eafe43b1c
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-20 12:59:37 -0500

    add banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
+import Banner from "../Banner/Banner"
 import Navbar from "../Navbar/Navbar"
 
 const Home = () => {
     return (
         <>
             <Navbar />
-            <h1>Home</h1>
+            <Banner />
         </>
     )
 }
