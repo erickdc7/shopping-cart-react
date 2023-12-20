751d6dac6780cfb717ca60106dbd2357bce7c094
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-20 13:47:28 -0500

    add products

---
+++ src/components/Products/Products.js
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { dataContext } from "../Context/DataContext"
+import "./Products.css"
+
+const Products = () => {
+    return (
+        <div>Products</div>
+    )
+}
+
+export default Products
