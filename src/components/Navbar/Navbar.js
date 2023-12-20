1cbd2f1f246584899837baa9233251626dbe212e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-20 12:55:30 -0500

    add styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 const Navbar = () => {
     return (
-        <div>Navbar</div>
-    )
-}
+        <div className="nav-container">
+            <nav className="navbar"></nav>
+            <h1 className="navbar-logo">Shop.</h1>
+            <h2>🛒</h2>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
