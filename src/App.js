ea52592c3e40d71c7729bc747246ed4e1e106c7d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-20 13:37:16 -0500

    add data provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CartContent from './components/CartContent/CartContent';
 import Home from './components/Home/Home';
+import DataProvider from './components/Context/DataContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<CartContent />} />
-      </Routes>
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<CartContent />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
