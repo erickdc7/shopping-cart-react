419265cea023e9c4af44ad9219b304fa191cca15
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-20 13:19:03 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CartContent from './components/CartContent/CartContent';
 import Home from './components/Home/Home';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<CartContent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
